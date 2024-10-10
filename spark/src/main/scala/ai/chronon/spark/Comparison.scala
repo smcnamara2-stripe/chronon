15db6abd93f240f483ade7ca1d17d7c4c6c09beb
Enrique Arango <earangol@stripe.com>
2024-10-10 17:31:36 -0400

    Handle Infinity and NaN in comparisons and parameterize double tolerance (#378)
    * Handle Infinity and NaN in comparisons and parameterize double tolerance
    
    * Add back file comments and package declaration
    
    * Clean up comparison
    
    * Revert unrelated formatting changes
    
    * More unrelated formatting
    
    * Fix comparison filters
    
    * Remove parentheses
    
    * Add option to include diff columns
    
    * Rename docs for consistency
    
    * Rename param
    
    * Update docs

--- spark/src/main/scala/ai/chronon/spark/Comparison.scala
+++ spark/src/main/scala/ai/chronon/spark/Comparison.scala
@@ -20,6 +20,7 @@ import org.slf4j.LoggerFactory
 import ai.chronon.online.Extensions.StructTypeOps
 import com.google.gson.Gson
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.expr
 import org.apache.spark.sql.types.{DecimalType, DoubleType, FloatType, MapType}
 
 import java.util
@@ -52,13 +53,25 @@ object Comparison {
     df.selectExpr(selects: _*)
   }
 
-  // Produces a "comparison" dataframe - given two dataframes that are supposed to have same data
-  // The result contains the differing rows of the same key
+  /**
+   * Produces a "comparison" dataframe - given two dataframes that are supposed to have same data.
+   * The result contains the differing rows of the same key.
+   * @param a first dataframe to compare
+   * @param b second dataframe to compare
+   * @param keys dataframe keys to use for comparison
+   * @param aName name that identifies the first dataframe
+   * @param bName name that identifies the second dataframe
+   * @param doubleTolerance tolerance for comparing double values (a.k.a. epsilon)
+   * @param includeDiffCols if true, the resulting comparison dataframe will include a "_diff" column for each column
+   *                        being compared, specifying if the column value differs between the two dataframes
+   */
   def sideBySide(a: DataFrame,
                  b: DataFrame,
                  keys: List[String],
                  aName: String = "a",
-                 bName: String = "b"): DataFrame = {
+                 bName: String = "b",
+                 doubleTolerance: Double = 0.00001,
+                 includeColumnDiffs: Boolean = false): DataFrame = {
 
     logger.info(
       s"""
@@ -82,38 +95,50 @@ object Comparison {
     var finalDf = joined
     val comparisonColumns =
       a.schema.fieldNames.toSet.diff(keys.toSet).toList.sorted
-    val colOrder =
-      keys.map(key => { finalDf(s"${aName}_$key").as(key) }) ++
-        comparisonColumns.flatMap { col =>
-          List(finalDf(s"${aName}_$col"), finalDf(s"${bName}_$col"))
-        }
     // double columns need to be compared approximately
     val doubleCols = a.schema.fields
       .filter(field =>
         field.dataType == DoubleType || field.dataType == FloatType || field.dataType.isInstanceOf[DecimalType])
       .map(_.name)
       .toSet
-    finalDf = finalDf.select(colOrder: _*)
     val comparisonFilters = comparisonColumns
-      .flatMap { col =>
+      .map { col => col -> {
         val left = s"${aName}_$col"
         val right = s"${bName}_$col"
         val compareExpression =
           if (doubleCols.contains(col)) {
-            s"($left is NOT NULL) AND ($right is NOT NULL) and (abs($left - $right) > 0.00001)"
-          } else { s"($left <> $right)" }
-        Seq(s"(($left IS NULL AND $right IS NOT NULL) OR ($right IS NULL AND $left IS NOT NULL) OR $compareExpression)")
+            val definedNotEqual = s"${isDefinedDouble(left)} AND ${isDefinedDouble(right)} AND abs($left - $right) > $doubleTolerance"
+            val undefinedNotEqual = s"${isUndefinedDouble(left)} AND $left <> $right"
+            s"(($definedNotEqual) OR ($undefinedNotEqual))"
+          } else s"($left <> $right)"
+        s"(($left IS NULL AND $right IS NOT NULL) OR ($right IS NULL AND $left IS NOT NULL) OR $compareExpression)"
       }
-    logger.info(s"Using comparison filter:\n  ${comparisonFilters.mkString("\n  ")}")
-    if (comparisonFilters.nonEmpty) {
-      finalDf.filter(comparisonFilters.mkString(" or "))
-    } else {
+    }
+    val comparisonMap = comparisonFilters.toMap
+    val colOrder =
+      keys.map(key => { finalDf(s"${aName}_$key").as(key) }) ++
+        comparisonColumns.flatMap { col => {
+          val cols = List(finalDf(s"${aName}_$col"), finalDf(s"${bName}_$col"))
+          if (includeColumnDiffs) cols :+ expr(s"${comparisonMap(col)}").as(s"${col}_diff") else cols
+        }
+      }
+    finalDf = finalDf.select(colOrder: _*)
+    logger.info(s"Using comparison filter:\n  ${comparisonFilters.map(_._2).mkString("\n  ")}")
+    if (comparisonFilters.nonEmpty) finalDf.filter(comparisonFilters.map(_._2).mkString(" OR ")) else {
       // all rows are good
       finalDf.filter("false")
     }
   }
 
-  private def prefixColumnName(df: DataFrame, prefix: String): DataFrame = {
+  private def isDefinedDouble(value: String): String = {
+    s"$value IS NOT NULL AND $value NOT IN (double('infinity'), double('-infinity'), double('NaN'))"
+  }
+
+  private def isUndefinedDouble(value: String): String = {
+    s"$value IN (double('infinity'), double('-infinity'), double('NaN'))"
+  }
+
+  private def prefixColumnName(df: DataFrame, prefix: String) = {
     val renamedColumns = df.columns.map(c => { df(c).as(s"$prefix$c") })
     df.select(renamedColumns: _*)
   }
